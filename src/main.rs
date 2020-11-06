f1a9259623f0186e99f02ab9a64f0ae90976c25c
Denilson <denilson020898@gmail.com>
2020-11-06 13:53:20 +0700

    basic tokio runtime inspected with simple logging

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-fn curry<F, T>(f: F, x: T) -> impl Fn(T) -> T
-where
-    T: Copy,
-    F: Fn(T, T) -> T,
-{
-    move |y| f(x, y)
-}
+use futures::future;
+use log::debug;
+use simplelog::{ConfigBuilder, LevelFilter, SimpleLogger};
 
 fn main() {
-    let add = |x, y| x + y;
-    let closure = curry(add, 5);
-    println!("closure(1) => {}", closure(1));
+    let config = ConfigBuilder::new()
+        .set_target_level(LevelFilter::Trace)
+        .build();
+    let _ = SimpleLogger::init(LevelFilter::Debug, config);
+
+    // let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = tokio::runtime::Builder::new()
+        .threaded_scheduler()
+        .core_threads(4)
+        .on_thread_start(|| debug!("on_thread_start()"))
+        .build()
+        .unwrap();
+
+    rt.enter(|| {
+        debug!("in rt.enter()");
+        tokio::spawn(future::lazy(|_| debug!("in tokio::spawn()")));
+    });
+    // tokio::spawn(future::lazy(|_| println!("in tokio::spawn")));
+    rt.spawn(future::lazy(|_| debug!("in rt.spawn()")));
+
+    rt.block_on(future::lazy(|_| debug!("in rt.block_on()")));
 }
